Adding thickness options for segmentation generation and fixing bug in deformation gradient calculation

utils.py
@@ -103,13 +103,8 @@ def thresholdModel(data,dataName,low,high,extract=True,cell=False):
     else:
         return pv.wrap(t.GetOutput())
 
-def parsePoint(inputData):
+def parsePoint(output_HnS):
 
-    cellId = inputData[0]
-    gaussId = inputData[1]
-    outputDir = inputData[2]
-
-    output_HnS = np.loadtxt(outputDir + '/HnS_out_python_'+str(cellId)+'_'+str(gaussId))
     J_curr =  np.linalg.det(np.reshape(output_HnS[-1,48:57], (3,3)))
     J_target = output_HnS[-1,46]/output_HnS[-1,47]
     # Change in volume from current to target
@@ -196,17 +191,17 @@ def getAneurysmValue(point,radius):
     Get the value of vessel behavior based on point location and radius                                                                                                                                      
     """
     zPt = point[2]
-    vesselValue = 0.65*np.exp(-abs(zPt/(1.5/4.0))**2)
+    vesselValue = 0.65*np.exp(-abs(zPt/(radius*4.0))**2)
     return vesselValue
 
 
-def getTEVGValue(point, radius):
+def getTEVGValue(point, radius, zcenter=0.0):
     """                                                                                                                                                                                                      
     Get the value of vessel behavior based on point location and radius                                                                                                                                      
     """
     value = 0.0
-    if abs(point[2]) <= radius:
-        value = 1.25*(0.5*np.cos((np.pi/radius)*point[2])+0.5) + 0.01
+    if abs(point[2]-zcenter) <= radius:
+        value = 1.25*(0.5*np.cos((np.pi/radius)*(point[2]-zcenter))+0.5) + 0.01
     return value
 
 def rotate_elastic_constants(C, A, tol=1e-4):
@@ -454,7 +449,7 @@ def computeGaussValues(mesh,name):
             J = np.matmul(Nxi,coordinates);
             Nx = np.matmul(np.linalg.inv(J),Nxi);
             gaussN = np.matmul(N,field)
-            gaussNx = np.ravel(np.matmul(Nx,field) + IdM)
+            gaussNx = np.ravel(np.transpose(np.matmul(Nx,field)) + IdM)
             allGaussN[q, g*3:(g+1)*3] = gaussN
             allGaussNx[q, g*9:(g+1)*9] = gaussNx
 
@@ -531,6 +526,12 @@ def alignContours(array1,array2):
             array3 = arrayTemp
 
     return array3
+
+def flipContour(array1):
+
+    array1 = np.flip(array1,axis=0)
+
+    return array1
 
 def interpolateSplineArray(array,numPts = 20,periodic = False,degree=3,redistribute=True):
     """

utils_run_vessel.py
@@ -9,8 +9,12 @@ from ctypes import c_double
 
 lib = cdll.LoadLibrary('./libgnr.so')
 run = lib.run
-run.argtypes = [c_char_p, c_char_p, c_int, c_int, c_int, c_int, c_double,c_double,c_double, c_double, c_double, c_double,c_double,c_double,c_double,c_double,c_double,c_double,c_double,c_double]
+run.argtypes = [c_char_p, c_char_p, c_int, c_int, c_int, c_int, \
+                c_double,c_double,c_double, c_double, c_double, \
+                c_double,c_double,c_double,c_double,c_double, \
+                c_double,c_double,c_double,c_double]
 #run(dir_prefix.encode('ascii'), filename.encode('ascii'),restart_arg,iter_arg,gnr_arg,num_days,step_size,sigma_arg,tauw_arg,anysm_arg,tevg_arg,F0, F1, F2, F3, F4,F5, F6, F7, F8)
+run.restype = c_int
 
 comm = MPI.COMM_WORLD
 size = comm.Get_size() # new: gives number of ranks in comm
@@ -19,20 +23,67 @@ rank = comm.Get_rank()
 input_file = open("input_array.dat","rb")
 input_array = pickle.load(input_file)
 input_file.close()
-
 numData = len(input_array)
-numDataPerRank = numData//size
-numRemainder = numData%size
 
-for i in range(numDataPerRank*rank, numDataPerRank*(rank+1)):
-    inputData = input_array[i]
+if rank == 0:
+
+
+    array_list = np.arange(numData)
+    output_array = np.zeros([numData,59]) #Create output array of same size
+    split = np.array_split(array_list,size) #Split input array by the number of available cores
+
+    split_sizes = []
+
+    for i in range(0,len(split),1):
+        split_sizes = np.append(split_sizes, len(split[i]))
+
+    split_sizes_input = split_sizes
+    displacements_input = np.insert(np.cumsum(split_sizes_input),0,0)[0:-1]
+
+    split_sizes_output = split_sizes*59
+    displacements_output = np.insert(np.cumsum(split_sizes_output),0,0)[0:-1]
+
+
+else:
+#Create variables on other cores
+    split_sizes_input = None
+    displacements_input = None
+    split_sizes_output = None
+    displacements_output = None
+    split = None
+    array_list = None
+    output_array = None
+
+
+split = comm.bcast(split, root=0) #Broadcast split array to other cores
+split_sizes = comm.bcast(split_sizes_input, root = 0)
+displacements = comm.bcast(displacements_input, root = 0)
+split_sizes_output = comm.bcast(split_sizes_output, root = 0)
+displacements_output = comm.bcast(displacements_output, root = 0)
+
+output_chunk = np.array([0] * len(split[rank])) #Create array to receive subset of data on each core, where rank specifies the core
+comm.Scatterv([array_list,split_sizes_input, displacements_input,MPI.DOUBLE],output_chunk,root=0)
+
+output = np.zeros([len(output_chunk),59]) #Create output array on each core
+
+for i in range(0,np.shape(output_chunk)[0],1):
+    inputData = input_array[output_chunk[i]]
+    out_array_type = c_double * 59  # equiv. to C double[3] type
+    out_array = out_array_type(*([0]*59))        # equiv. to double arr[3] = {...} instance
     prefix = inputData[2].encode('ascii')
     suffix = ('python_'+str(inputData[0])+'_'+str(inputData[1])).encode('ascii')
-    run(prefix, suffix, inputData[3], inputData[4], inputData[5],inputData[6], inputData[7], inputData[8], inputData[9], inputData[10], inputData[11], inputData[12], inputData[13],inputData[14],inputData[15],inputData[16],inputData[17],inputData[18], inputData[19], inputData[20])
+    run(prefix, suffix, inputData[3], inputData[4], inputData[5],inputData[6], inputData[7],\
+        inputData[8], inputData[9], inputData[10], inputData[11], inputData[12], inputData[13],\
+        inputData[14],inputData[15],inputData[16],inputData[17],inputData[18], inputData[19],\
+        inputData[20],out_array)
+    output[i] = np.array(out_array)
 
-if rank < numRemainder:
-    i = numDataPerRank*size + rank
-    inputData = input_array[i]
-    prefix = inputData[2].encode('ascii')
-    suffix = ('python_'+str(inputData[0])+'_'+str(inputData[1])).encode('ascii')
-    run(prefix, suffix, inputData[3], inputData[4], inputData[5],inputData[6], inputData[7], inputData[8], inputData[9], inputData[10], inputData[11], inputData[12], inputData[13],inputData[14],inputData[15],inputData[16],inputData[17],inputData[18], inputData[19], inputData[20])
+comm.Barrier()
+
+comm.Gatherv(output,[output_array,split_sizes_output,displacements_output,MPI.DOUBLE], root=0) #Gather output data together
+
+if rank == 0:
+    parse_file = open("parse_array.dat","wb")
+    pickle.dump(output_array, parse_file)
+    parse_file.close()
+

vessel.py
@@ -20,7 +20,7 @@ class Vessel():
         self.tau_h = None
 
         self.nG = 8
-        self.max_days = kwargs.get("gnrMaxDays", 720)
+        self.max_days = kwargs.get("max_days", 720)
         self.prefix = kwargs.get("prefix", "./")
         self.numRad = kwargs.get("numRad", 4)
         self.numCirc = kwargs.get("numCirc", 12)
@@ -44,7 +44,7 @@ class Vessel():
         self.outputDir = kwargs.get("outputDir","Outputs")
         self.estimateWSS = kwargs.get("estimateWSS", False)
         self.predictMethod = kwargs.get("predictMethod", "aitken")
-        self.gnr_step_size = kwargs.get("gnrStepSize", 1.0)
+        self.gnr_step_size = kwargs.get("gnr_step_size", 1.0)
         self.damping = kwargs.get("damping", 1e4)
         self.penalty = kwargs.get("penalty", 1e7)
         self.startTime = 0.0
@@ -53,7 +53,10 @@ class Vessel():
         self.segmentationName = kwargs.get("segmentationName", None)
         self.estimateOuterSegmentation = kwargs.get("estimateOuterSegmentation",False)
         self.thicknessRatio = kwargs.get("thicknessRatio",0.1)
+        self.constantThickness = kwargs.get("constantThickness",False)
         self.adaptiveMesh = kwargs.get("adaptiveMesh",False)
+        self.rotation_matrix = np.eye(3)
+        self.zcenter = 0.0
 
     def writeStatus(self, currTime):
         with open('svDriverIterations','a') as f:
@@ -219,15 +222,11 @@ class Vessel():
         time2 = time.time()
         print("Time to run_vessel: " + str(time2 - time1))
 
+        parse_file = open("parse_array.dat","rb")
+        parse_array = pickle.load(parse_file)
+        parse_file.close()
+
         print("Parsing points...")
-        pool = Pool()
-        # Begin processing
-        output_array = []
-        for output in tqdm(pool.imap(parsePoint,input_array), total=len(input_array)):
-            output_array.append(output)
-        pool.close()
-        pool.join()
-        pool.terminate()
 
         for q in range(numCells):
             stiffness_mem_g = []
@@ -235,7 +234,23 @@ class Vessel():
             J_target_g = []
             J_curr_g = []
             for p in range(self.nG):
-                J_target, J_curr, stiffness, sigma, wss, sigma_inv = output_array[q*self.nG + p]
+
+                output_data = parse_array[q*self.nG + p]
+
+                J_curr =  np.linalg.det(np.reshape(output_data[48:57], (3,3)))
+                J_target = output_data[46]/output_data[47]
+                # Change in volume from current to target
+                # J_di = J_target/J_curr
+                
+                stiffness = output_data[1:37]
+                sigma = output_data[37:46]
+
+                # Dont forget units 
+                stiffness = np.array(stiffness)*10.0
+                sigma = np.array(sigma)*10.0
+
+                sigma_inv = output_data[57]
+                wss = output_data[58]
 
                 stiffness_mem_g = stiffness_mem_g + stiffness.tolist()
                 sigma_mem_g = sigma_mem_g + sigma.tolist()
@@ -478,8 +493,8 @@ class Vessel():
                 self.omega = -self.omega*np.dot(rprev,diff)/np.dot(diff,diff)
             else:
                 self.omega = 0.1
-        elif self.predictMethod == "reduced":
-            self.predictMethod = "aitken"
+            if np.abs(self.omega) > 1:
+                self.omega = np.sign(self.omega)
 
         # Calculate cauchy green tensor
         for q in range(numPts):
@@ -805,6 +820,8 @@ class Vessel():
         self.numLen = int(np.max([1,round((self.numLen) / (numSegsInner-1))]) * (numSegsInner-1))
 
         #Align inner data to each other
+        #Flip first contour to prevent negative jacobian (seems necessary on only come ctgrs?)
+        ctgrPtsInner[0] = flipContour(ctgrPtsInner[0])
         for i in range(np.shape(ctgrPtsInner)[0]-1):
             ctgrPtsInner[i+1] = alignContours(ctgrPtsInner[i],ctgrPtsInner[i+1])
 
@@ -837,6 +854,7 @@ class Vessel():
         self.tau_h = nativeIn[13][1]
 
         #Build material array (constant for now)
+
         materialArray = [ nativeIn[7][0],nativeIn[4][0],nativeIn[4][1],nativeIn[4][2],nativeIn[2][0]*10.0,0,0,0,0,0,0,0,0,0,\
                           nativeIn[7][2],nativeIn[5][2],nativeIn[2][4]*10.0,nativeIn[2][5],0,0,0,0,0,0,0,0,0,0,\
                           nativeIn[7][3],nativeIn[5][3],nativeIn[2][6]*10.0,nativeIn[2][7],0,0,0,0,0,0,0,0,0,0,\
@@ -886,12 +904,16 @@ class Vessel():
                     else:
                         p_radius = np.linalg.norm(ctgrPtsInner[i,j%self.numCirc,:] - center)
                         p_v_r = (ctgrPtsInner[i,j%self.numCirc,:] - center)/p_radius
-                        p_thickness = p_radius*self.thicknessRatio
+                        if self.constantThickness:
+                            p_thickness = self.thickness
+                        else:
+                            p_thickness = p_radius*self.thicknessRatio
                         xPt = (k/self.numRad)*(p_thickness*p_v_r[0]) + ctgrPtsInner[i,j%self.numCirc,0]
                         yPt = (k/self.numRad)*(p_thickness*p_v_r[1]) + ctgrPtsInner[i,j%self.numCirc,1]
                         zPt = (k/self.numRad)*(p_thickness*p_v_r[2]) + ctgrPtsInner[i,j%self.numCirc,2]
                     structureIds[i*(self.numCirc+1)*(self.numRad+1) + j*(self.numRad+1) + k] = i*(self.numCirc)*(self.numRad+1) + (j%self.numCirc)*(self.numRad+1) + k + 1
-                    points[i*(self.numCirc+1)*(self.numRad+1) + j*(self.numRad+1) + k,:] = [xPt, yPt, zPt]
+                    pointCoordinate = np.matmul(self.rotation_matrix, np.array([xPt, yPt, zPt]))
+                    points[i*(self.numCirc+1)*(self.numRad+1) + j*(self.numRad+1) + k,:] = pointCoordinate
 
 
         coords=[[0, 1, 0],
@@ -923,15 +945,17 @@ class Vessel():
 
                     v_r = np.mean(cellPts[[1,2,5,6]],axis=0) - np.mean(cellPts[[0,3,4,7]],axis=0)
                     v_r = v_r/np.linalg.norm(v_r)
-                    v_t = np.mean(cellPts[[0,1,2,3]],axis=0) - np.mean(cellPts[[4,5,6,7]],axis=0)
+                    v_t = np.mean(cellPts[[4,5,6,7]],axis=0) - np.mean(cellPts[[0,1,2,3]],axis=0)
                     v_t = v_t/np.linalg.norm(v_t)
-
-                    v_z = np.cross(v_r,v_t)
+                    v_z = np.mean(cellPts[[2,3,6,7]],axis=0) - np.mean(cellPts[[0,1,4,5]],axis=0)
                     v_z = v_z/np.linalg.norm(v_z)
-                    v_t = np.cross(v_z,v_r)
-                    v_t = v_t/np.linalg.norm(v_t)
-                    #v_z = np.mean(cellPts[[1,2,5,6]],axis=0) - np.mean(cellPts[[0,3,4,7]],axis=0)
-                    #v_z = v_z/np.linalg.norm(v_z)
+
+                    v_r = np.cross(v_z,v_t)
+                    v_r = -v_r/np.linalg.norm(v_r)
+                    #v_t = np.cross(v_z,v_r)
+                    #v_t = v_t/np.linalg.norm(v_t)
+                    v_z = np.cross(v_t,v_r)
+                    v_z = -v_z/np.linalg.norm(v_z)
 
                     e_r[i*self.numCirc*self.numRad + j*self.numRad + k,:] = v_r
                     e_t[i*self.numCirc*self.numRad + j*self.numRad + k,:] = v_t
@@ -963,7 +987,18 @@ class Vessel():
                     solidStressQueryIds[k + j*self.numRad + i*self.numRad*self.numCirc] = j*self.numRad + i*self.numRad*self.numCirc + self.numRad
 
                     if self.tevg:
-                        tevgValue[k + j*self.numRad + i*self.numRad*self.numCirc] = getTEVGValue([xPt,yPt,zPt], self.radius)
+                        tevgValue[k + j*self.numRad + i*self.numRad*self.numCirc] = getTEVGValue([xPt,yPt,zPt], self.radius, self.zcenter)
+                        if tevgValue[k + j*self.numRad + i*self.numRad*self.numCirc] > 0:
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,4] = 200000000
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,1] = 1
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,2] = 1
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,3] = 1
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,14] = 0
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,28] = 0
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,42] = 0
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,56] = 0
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,70] = 0
+                            e_ma[k + j*self.numRad + i*self.numRad*self.numCirc,84] = 0
                     if self.aneurysm:
                         aneurysmValue[k + j*self.numRad + i*self.numRad*self.numCirc] = getAneurysmValue([xPt,yPt,zPt], self.radius)
 