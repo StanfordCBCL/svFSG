ec9a245bc34369663f11a0bab69228e674b8f1f2
Erica Leigh Schwarz <schwarze@login02.expanse.sdsc.edu>
2023-02-01 20:09:52 -0800

    Adding cvessel class

---
+++ cvessel.py
@@ -0,0 +1,16 @@
+class cvessel():
+    def __init__(self):
+        self.savestring = ""
+        self.prefix = ""
+        self.name = ""
+        self.restart = 0
+        self.iteration = 0
+        self.simulate = 0
+        self.num_days = 0
+        self.step_size = 0
+        self.sigma_inv = 0
+        self.tauw_wss = 0
+        self.aneurysm = 0
+        self.tevg = 0
+        self.F = [0]*9
+        self.out_array = [0]*59
