f309f9a5c1a566fc2ef0c4af864e03e4ee9de4a9
Erica Schwarz <els94@login1.grace.ycrc.yale.edu>
2023-12-13 15:11:22 -0500

    adding torus and new aneurysm

--- run_aorta.py
+++ run_aorta.py
@@ -2,6 +2,7 @@ import vessel
 import pickle
 import os
 import time
+import numpy as np
 
 def saveVessel(vess):
     with open('vessel.pickle', 'wb') as file:
@@ -20,51 +21,48 @@ if os.path.exists('vessel.pickle'):
     simulation_vessel = loadVessel()
     simulation_vessel.startTime = simulation_vessel.currTime
 else:
-    simulation_vessel = vessel.Vessel(vesselType="segmentation", segmentationName="aorta_fsg", numLen=48, numCirc=24)
-    simulation_vessel.estimateOuterSegmentation = True
-    simulation_vessel.thicknessRatio = 0.0795
-    simulation_vessel.outletPressure = 1333.3*100*1.4
+    simulation_vessel = vessel.Vessel(radius=0.0647, thickness=0.004, length=1.5, numLen=48, numCirc=24, numRad=4)
+    simulation_vessel.outletPressure = 1333.22*105
     simulation_vessel.inletFlow = -97.0
-    simulation_vessel.gnr_step_size = 20
-    simulation_vessel.gnr_max_days = 7200
-    simulation_vessel.numProcessorsSolid = 48
-    simulation_vessel.numProcessorsFluid = 128
-    simulation_vessel.damping = 1e4
-    simulation_vessel.penalty = 5e8
+    simulation_vessel.gnr_step_size = 1
+    simulation_vessel.gnr_max_days = 720
+    simulation_vessel.damping = 1e7
+    simulation_vessel.penalty = 1e9
+    simulation_vessel.tolerance = 1e-3
     simulation_vessel.simulationExecutable = "~/svFSI-build/svFSI-build/mysvfsi"
-    simulation_vessel.flipInlet = True
     simulation_vessel.setInputFileValues()
+    simulation_vessel.numProcessorsSolid = 72
     simulation_vessel.smoothAttributesValue = 0.1
+    simulation_vessel.aneurysm = 1
+    simulation_vessel.zod = 0.3
+    simulation_vessel.zapex = 0
+    simulation_vessel.thetaod = np.pi/2
+    simulation_vessel.thetaapex = np.pi
     os.system('mkdir -p ' + simulation_vessel.outputDir)
     os.system('mkdir -p ' + 'meshIterations')
     os.system('mkdir -p ' + 'meshResults')
     os.system('mkdir -p ' + 'simulationResults')
+    os.system('mkdir -p ' + 'materialResults')
     simulation_vessel.initializeVessel()
-    simulation_vessel.runFluidIteration()
+    simulation_vessel.estimateFluidIteration()
     simulation_vessel.runSolidIteration()
+    simulation_vessel.estimateFluidIteration()
     simulation_vessel.runMaterialIteration()
     simulation_vessel.currTime = time.time() - startTime + simulation_vessel.startTime
-    simulation_vessel.writeStatus(simulation_vessel.currTime, "FSG")
+    simulation_vessel.writeStatus(simulation_vessel.currTime, "SG")
     simulation_vessel.incrementIteration()
     saveVessel(simulation_vessel)
 
 while simulation_vessel.timeStep < simulation_vessel.total_time_steps:
-    while simulation_vessel.residual > simulation_vessel.tolerance or simulation_vessel.timeIter < 3:
-        while simulation_vessel.residual > simulation_vessel.tolerance:
-            simulation_vessel.runSolidIteration()
-            simulation_vessel.skipFluidIteration()
-            simulation_vessel.runMaterialIteration()
-            simulation_vessel.currTime = time.time() - startTime + simulation_vessel.startTime
-            simulation_vessel.writeStatus(simulation_vessel.currTime, "SG")
-            simulation_vessel.incrementIteration()
-            saveVessel(simulation_vessel)
-        simulation_vessel.runFluidIteration()
+    while simulation_vessel.residual > simulation_vessel.tolerance or simulation_vessel.timeIter < 5:
         simulation_vessel.runSolidIteration()
+        simulation_vessel.estimateFluidIteration()
         simulation_vessel.runMaterialIteration()
         simulation_vessel.currTime = time.time() - startTime + simulation_vessel.startTime
-        simulation_vessel.writeStatus(simulation_vessel.currTime, "FSG")
+        simulation_vessel.writeStatus(simulation_vessel.currTime)
         simulation_vessel.incrementIteration()
         saveVessel(simulation_vessel)
+    simulation_vessel.runSystoleDiastole()
     simulation_vessel.timeIter = 0
     simulation_vessel.residual = simulation_vessel.tolerance*10.0
     simulation_vessel.incrementTimestep()
